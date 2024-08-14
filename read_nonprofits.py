b2d20f96d7d0026d05ca50a1275059351f4185ff
Kate Cowie <katecowie@Kathryns-MacBook-Air.local>
2024-08-14 15:02:55 -0400

    filter by NTEE code

--- read_nonprofits.py
+++ read_nonprofits.py
@@ -21,14 +21,40 @@ def construct_url(params:dict):
         url += f"ntee%5Bid%5D=D=={params['ntee']}"
     return url
 
-organization = input('Organization: ')
-state = input('State: ')
-params = {'organization': organization, 'state':state, 'ntee': None}
-api_url = construct_url(params)
-print(f"Request url: {api_url}\n")
 
-res = re.get(api_url)
-print("Status: ", res.status_code)
-data = res.json()
-for o in data["organizations"]:
-    print(f"{o['name'], ({o['ntee_code']})")
+ntee_codes = ['B01', 'E01', 'E30Z', 'E86', 'E70', 'F01', 'G01', 'G012', 'G20', 'G46Z', 'G50', 'G80', 'H02', 'H12', 'H50', 'H200','S41']
+
+def prepare_api_request()-> str:
+    organization = input('Organization: ')
+    # state = input('State: ')
+    params = {'organization': organization, 'state':None, 'ntee': None}
+    api_url = construct_url(params)
+    return api_url
+
+def call_propublica_api(api_url:str) -> dict:
+    res = re.get(api_url)
+    print("Response Status:", res.status_code)
+    data = res.json()
+    print(f"Raw results: {data['total_results']}")
+    pretty = json.dumps(data, indent=4) 
+    return data
+
+def filter_by_ntee_code(response_data:dict)-> list:
+    filtered_organizations = [
+        o for o in response_data["organizations"] if o['ntee_code'] in ntee_codes
+    ]
+    for o in filtered_organizations:
+        print(f"{o['name']}, (ntee:{o['ntee_code']}), {o['ein']}")
+    print(f"Filtered results: {len(filtered_organizations)}")
+    print(type(filtered_organizations))
+    return filtered_organizations
+
+def main():
+    api_url = prepare_api_request()
+    data = call_propublica_api(api_url)
+    organizations = filter_by_ntee_code(data)
+
+if __name__=="__main__":
+    main()
+
+   
